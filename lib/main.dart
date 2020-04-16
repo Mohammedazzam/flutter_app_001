33ac45ac153acb62feccdc416b6f7e1cc6957e5e
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-16 13:46:49 +0300

    4.3(استدعاء ال widget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 main(){
-  runApp()
+  runApp(Flutter001());
 }
 
 class Flutter001 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return null();
   }
 }
