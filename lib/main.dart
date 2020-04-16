190dace116e357d0144671cc8ad935268f4c72ad
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-16 13:44:58 +0300

    4.2(عمل ال stless)

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 main(){
   runApp()
 }
+
+class Flutter001 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
